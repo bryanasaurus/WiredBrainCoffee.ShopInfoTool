87de51508d960d50adff2038bc32c21dc7e5f4ff
bryanasaurus <bryan.morrish@gmail.com>
2020-05-14 15:42:28 +0100

    Introduce command handlers

--- WireBrainCoffee.ShopInfoTool/Program.cs
+++ WireBrainCoffee.ShopInfoTool/Program.cs
@@ -13,43 +13,19 @@ namespace WireBrainCoffee.ShopInfoTool
 
             var coffeeShopDataProvider = new CoffeeShopDataProvider();
 
+
+
             while (true)
             {
                 var line = Console.ReadLine();
                 var coffeeShops = coffeeShopDataProvider.LoadCoffeeShops();
-                if (string.Equals("help", line, StringComparison.OrdinalIgnoreCase))
-                {
-                    Console.WriteLine("> Available coffee shop commands:");
-                    foreach (var coffeeShop in coffeeShops)
-                    {
-                        Console.WriteLine($"> " + coffeeShop.Location);
-                    }
-                }
-                else
-                {
-                    var foundCoffeeShops = coffeeShops
-                        .Where(x => x.Location.StartsWith(line, StringComparison.OrdinalIgnoreCase))
-                        .ToList();
-
-                    if(foundCoffeeShops.Count == 0)
-                    {
-                        Console.WriteLine($"> Command '{line}' not found");
-                    }
-                    else if (foundCoffeeShops.Count == 1)
-                    {
-                        var coffeeShop = foundCoffeeShops.Single();
-                        Console.WriteLine($"> Location: {coffeeShop.Location}");
-                        Console.WriteLine($"> Beans in stock: {coffeeShop.BeansInStockInKg} kg");
-                    }
-                    else
-                    {
-                        Console.WriteLine($"> Multiple matching coffee shop commands found: ");
-                        foreach(var coffeeType in foundCoffeeShops)
-                        {
-                            Console.WriteLine($"> {coffeeType.Location}");
-                        }
-                    }
-                }
+                
+                var commandHandler= // command handler can be HelpCommandHandler or CoffeeShopCommandHandler
+                string.Equals("help", line, StringComparison.OrdinalIgnoreCase)
+                    ? new HelpCommandHandler(coffeeShops) as ICommandHandler
+                    : new CoffeeShopCommandHandler(coffeeShops, line);
+                
+                commandHandler.HandleCommand();
             }
         }
     }
