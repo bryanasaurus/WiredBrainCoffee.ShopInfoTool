30bc2bdf1643f8aa6e347707852659ccb734db38
bryanasaurus <bryan.morrish@gmail.com>
2020-05-14 15:07:46 +0100

    add paper cups in stock propert

--- WiredBrainCoffee.DataAccess/CoffeeShopDataProvider.cs
+++ WiredBrainCoffee.DataAccess/CoffeeShopDataProvider.cs
@@ -7,9 +7,9 @@ namespace WiredBrainCoffee.DataAccess
     {
         public IEnumerable<CoffeeShop> LoadCoffeeShops()
         {
-            yield return new CoffeeShop { Location = "Frankfurt", BeansInStockInKg = 107 };
-            yield return new CoffeeShop { Location = "Freiburg", BeansInStockInKg = 23 };
-            yield return new CoffeeShop { Location = "Munich", BeansInStockInKg = 56 };
+            yield return new CoffeeShop { Location = "Frankfurt", BeansInStockInKg = 107, PaperCups = 350 };
+            yield return new CoffeeShop { Location = "Freiburg", BeansInStockInKg = 23, PaperCups = 200 };
+            yield return new CoffeeShop { Location = "Munich", BeansInStockInKg = 56, PaperCups = 12 };
         }
     }
 }
